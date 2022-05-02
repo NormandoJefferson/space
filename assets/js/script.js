221f3caf4a253f1cd4448a6811b7ecb633a022a5
NormandoJefferson <normandojefferson@hotmail.com>
2022-05-02 15:36:06 -0300

    Movimentacao inicial da nave

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const yourShip = document.querySelector('.player-shooter');
+const playArea = document.querySelector('#main-play-game');
+
+// Movimento e tiro da nave.
+function flyShip(event) {
+    if(event.key === 'ArrowUp') {
+        event.preventDefault();
+        moveUp();
+    }else if(event.key === 'ArrowDown') {
+        event.preventDefault();
+        moveDown();
+    }else if(event.key === " ") {
+        event.preventDefault();
+        fireLaser();
+    }
+}
+
+//Sobe a nave.
+function moveUp() {
+    // Busca os estilos da nossa nave e retorna o valor top na forma de string.
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    
+    if(topPosition === "0px") {
+        return;
+    }else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`
+    }
+}
